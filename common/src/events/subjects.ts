d4e354f04eeaf41623d6a6675896363333542b4e
rovilay <rovilayjnr01@gmail.com>
2024-11-23 11:27:26 -0500

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -1,5 +1,9 @@
 export enum Subjects {
     TicketCreated = 'ticket:created',
     TicketUpdated = 'ticket:updated',
+
+
+    OrderCreated = 'order:created',
+    OrderCancelled = 'order:cancelled',
     OrderUpdated = 'order:updated'
 }
