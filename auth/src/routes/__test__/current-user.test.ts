080000ad860a090fc9ddc72ce432c7662f920770
rovilay <rovilayjnr01@gmail.com>
2024-11-23 11:53:05 -0500

    - ticket and order services

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -4,10 +4,6 @@ import { app } from '../../app';
 it('responds with details about the current user', async () => {
   const cookie = await global.signin();
 
-  if (!cookie) {
-    throw new Error("Cookie not set after signup")
-  }
-
   const response = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
