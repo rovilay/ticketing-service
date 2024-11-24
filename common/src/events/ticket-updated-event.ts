bf52ade5747a392cb01c8943618fc133cfd12007
rovilay <rovilayjnr01@gmail.com>
2024-11-24 07:07:56 -0500

    Updates

--- common/src/events/ticket-updated-event.ts
+++ common/src/events/ticket-updated-event.ts
@@ -5,6 +5,7 @@ export interface TicketUpdatedEvent {
     data: {
         id: string;
         version: number;
+        orderId?: string;
         title: string;
         price: number;
         userId: string;
